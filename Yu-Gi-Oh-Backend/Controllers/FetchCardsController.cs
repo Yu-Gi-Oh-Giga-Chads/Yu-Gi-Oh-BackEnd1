25b9bef0e516cf58725407f434d6dc77b35c8fd4
IvayloKartev <ivaylokartev@gmail.com>
2024-06-04 16:11:48 +0100

    Imported libraries and added FetchCards controller

---
+++ Yu-Gi-Oh-Backend/Controllers/FetchCardsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Yu_Gi_Oh_Backend.Controllers
+{
+    public class FetchCardsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
